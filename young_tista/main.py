98c9c59889fc5dbf06e7663fbee1df7c6d8e52c8
Batista Harahap <batista@bango29.com>
2022-12-01 12:20:52 +0400

    initial commit

---
+++ young_tista/main.py
@@ -0,0 +1,35 @@
+import os
+import openai
+
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def ask(question: str = typer.Option(None, prompt=True)):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    with open('model.txt', 'r') as f:
+        prompt = f.read()
+        prompt += f"{question}\n"
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=750,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    choices = response.get('choices')
+    for choice in choices:
+        text = choice.get('text').replace('Young Tista:\n', '').replace('Young Tista:', '').strip()
+        print(f"Young Tista: {text}")
+        print('\n')
+
+
+if __name__ == "__main__":
+    app()
