d7e21284188cf5e5f141461b575eca60eddb3f82
Batista Harahap <batista@bango29.com>
2022-12-01 18:59:26 +0400

    monkeypatched asynciomotorclient's method of getting the asyncio loop

--- young_tista/api/shared.py
+++ young_tista/api/shared.py
@@ -1,3 +1,4 @@
+import asyncio
 from os import environ
 
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -9,6 +10,10 @@ class AIOEngineDependency:
     def __init__(self):
         mongo_url = environ.get("MONGO_URL")
         client = AsyncIOMotorClient(mongo_url)
+
+        # Monkey patched, here's why: https://twitter.com/tista/status/1595658624449257472
+        client.get_io_loop = asyncio.get_running_loop
+
         self.engine = AIOEngine(
             client=client,
             database="young-tista"
